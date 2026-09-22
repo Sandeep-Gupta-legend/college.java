c248720e4f6e2193e3bcebe00e77c9d26025415a
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2026-09-22 09:50:06 +0530

    second student class with hello

---
+++ Test2.java
@@ -0,0 +1,12 @@
+class Student {
+    void display() {
+        System.out.println("Hello");
+    }
+}
+
+public class Test2 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.display();
+    }
+}
